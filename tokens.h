c486df7d949c37eeff496253530964bb2cfada6c
michealzs <seuntuyie@gmail.com>
2024-01-13 06:43:29 -0500

    Initial commit

---
+++ tokens.h
@@ -0,0 +1,16 @@
+// CMSC 430 Compiler Theory and Design
+// Project 1 Skeleton
+// UMGC CITE
+// Summer 2023
+
+// This file contains the enumerated type definition for tokens
+
+
+enum Tokens {
+    ADDOP = 256, MULOP, ANDOP, RELOP, ARROW, BEGIN_, CASE, CHARACTER, END,
+    ENDSWITCH, FUNCTION, INTEGER, IS, LIST, OF, OTHERS, RETURNS, SWITCH, WHEN,
+    IDENTIFIER, INT_LITERAL, CHAR_LITERAL,
+    ELSE, ELSIF, ENDFOLD, ENDIF, FOLD, IF, LEFT, REAL, RIGHT, THEN,
+    OROP, NOTOP, REMOP, EXPOP, NEGOP, HEX_LITERAL, DECIMAL_LITERAL
+};
+
